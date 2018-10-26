a16048d2719b6185164f639bfb41365ed9a07973
Hadar Pur <hpur@liveperson.com>
2018-10-26 18:44:18 +0300

    main animation fixed

--- Minesweeper/UI/MainViewController.swift
+++ Minesweeper/UI/MainViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class MainViewController: UIViewController {
     var firstAsk = true
     var mGameViewController: GameViewController?
-
+    var firstShow: Bool = true
     @IBOutlet weak var mEazyBtn: UIButton!
     @IBOutlet weak var mNormalBtn: UIButton!
     @IBOutlet weak var mHardBtn: UIButton!
@@ -26,46 +26,71 @@ class MainViewController: UIViewController {
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        mEazyBtn.center.x  -= view.bounds.width
-        mNormalBtn.center.x  -= view.bounds.width
-        mHardBtn.center.x  -= view.bounds.width
-        mRecordsBtn.center.x  -= view.bounds.width
-        mInstructionBtn.center.x  -= view.bounds.width
+        if (firstShow) {
+            mEazyBtn.center.x  -= view.bounds.width
+            mNormalBtn.center.x  -= view.bounds.width
+            mHardBtn.center.x  -= view.bounds.width
+            mRecordsBtn.center.x  -= view.bounds.width
+            mInstructionBtn.center.x  -= view.bounds.width
+        }
+        else {
+            mEazyBtn.center.x  += view.bounds.width
+            mNormalBtn.center.x  += view.bounds.width
+            mHardBtn.center.x  += view.bounds.width
+            mRecordsBtn.center.x  += view.bounds.width
+            mInstructionBtn.center.x  += view.bounds.width
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        UIView.animate(withDuration: 0.5, delay: 0.05, options: [],
-                       animations: {
-            self.mEazyBtn.center.x  += self.view.bounds.width
+        UIView.animate(withDuration: 0.5, delay: 0.05, options: [], animations: {
+            if (self.firstShow) {
+                self.mEazyBtn.center.x  += self.view.bounds.width
+            }
+            else{
+                self.mEazyBtn.center.x  -= self.view.bounds.width
+            }
         }, completion: nil)
-        UIView.animate(withDuration: 0.5, delay: 0.1, options: [],
-                       animations: {
-            self.mNormalBtn.center.x  += self.view.bounds.width
+        UIView.animate(withDuration: 0.5, delay: 0.1, options: [], animations: {
+            if (self.firstShow) {
+                self.mNormalBtn.center.x  += self.view.bounds.width
+            }
+            else{
+                self.mNormalBtn.center.x  -= self.view.bounds.width
+            }
         }, completion: nil)
-        UIView.animate(withDuration: 0.5, delay: 0.15, options: [],
-                       animations: {
-            self.mHardBtn.center.x  += self.view.bounds.width
-
+        UIView.animate(withDuration: 0.5, delay: 0.15, options: [], animations: {
+           if (self.firstShow) {
+                self.mHardBtn.center.x  += self.view.bounds.width
+           }
+            else{
+                self.mHardBtn.center.x  -= self.view.bounds.width
+            }
         }, completion: nil)
-        UIView.animate(withDuration: 0.5, delay: 0.2, options: [],
-                       animations: {
-            self.mRecordsBtn.center.x  += self.view.bounds.width
+        UIView.animate(withDuration: 0.5, delay: 0.2, options: [],animations: {
+            if (self.firstShow) {
+                self.mRecordsBtn.center.x  += self.view.bounds.width
+            }
+            else{
+                self.mRecordsBtn.center.x  -= self.view.bounds.width
+            }
         }, completion: nil)
-        UIView.animate(withDuration: 0.5, delay: 0.25, options: [],
-                       animations: {
-            self.mInstructionBtn.center.x  += self.view.bounds.width
+        UIView.animate(withDuration: 0.5, delay: 0.25, options: [],animations: {
+            if (self.firstShow) {
+                self.mInstructionBtn.center.x  += self.view.bounds.width
+            }
+            else{
+                self.mInstructionBtn.center.x  -= self.view.bounds.width
+            }
         }, completion: nil)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        firstShow = false
     }
 
     @IBAction func eazyLevelBtn(_ sender: UIButton) {
         sender.touch()
         
-        let deadlineTime = DispatchTime.now() + .milliseconds(30)
+        let deadlineTime = DispatchTime.now() + .milliseconds(500)
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         self.mGameViewController = storyBoard.instantiateViewController(withIdentifier: "GameViewController") as? GameViewController
         
@@ -74,14 +99,15 @@ class MainViewController: UIViewController {
         }
         
         self.mGameViewController!.mDiff = 0
-        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: { self.navigationController?.pushViewController(self.mGameViewController!, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: {
+            self.navigationController?.pushViewController(self.mGameViewController!, animated: true)
         })
     }
     
     @IBAction func normalLevelBtn(_ sender: UIButton) {
         sender.touch()
         
-        let deadlineTime = DispatchTime.now() + .milliseconds(30)
+        let deadlineTime = DispatchTime.now() + .milliseconds(500)
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         self.mGameViewController = storyBoard.instantiateViewController(withIdentifier: "GameViewController") as? GameViewController
         
@@ -93,14 +119,15 @@ class MainViewController: UIViewController {
 //        self.mGameViewController!.mGameBoard.size
 //        self.mGameViewController!.mGameBoard.numberOfItems(inSection: 10)
 
-        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: { self.navigationController?.pushViewController(self.mGameViewController!, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: {
+            self.navigationController?.pushViewController(self.mGameViewController!, animated: true)
         })
     }
     
     @IBAction func hardLevelBtn(_ sender: UIButton) {
         sender.touch()
         
-        let deadlineTime = DispatchTime.now() + .milliseconds(30)
+        let deadlineTime = DispatchTime.now() + .milliseconds(500)
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         self.mGameViewController = storyBoard.instantiateViewController(withIdentifier: "GameViewController") as? GameViewController
         
@@ -109,14 +136,15 @@ class MainViewController: UIViewController {
         }
         
         self.mGameViewController!.mDiff = 2
-        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: { self.navigationController?.pushViewController(self.mGameViewController!, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: {
+            self.navigationController?.pushViewController(self.mGameViewController!, animated: true)
         })
     }
     
     @IBAction func recordsBtn(_ sender: UIButton) {
         sender.touch()
 
-        let deadlineTime = DispatchTime.now() + .milliseconds(30)
+        let deadlineTime = DispatchTime.now() + .milliseconds(500)
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let recordsViewController = storyBoard.instantiateViewController(withIdentifier: "RecordsViewController") as? RecordsViewController
         
@@ -124,14 +152,15 @@ class MainViewController: UIViewController {
             return
         }
         
-        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: { self.navigationController?.pushViewController(recordsViewController!, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: {
+            self.navigationController?.pushViewController(recordsViewController!, animated: true)
         })
     }
     
     @IBAction func instructionsBtn(_ sender: UIButton) {
         sender.touch()
         
-        let deadlineTime = DispatchTime.now()
+        let deadlineTime = DispatchTime.now() + .milliseconds(500)
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let instrucionsViewController = storyBoard.instantiateViewController(withIdentifier: "InstructionsViewController") as? InstructionsViewController
         
@@ -139,7 +168,8 @@ class MainViewController: UIViewController {
             return
         }
         
-        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: { self.navigationController?.pushViewController(instrucionsViewController!, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: {
+            self.navigationController?.pushViewController(instrucionsViewController!, animated: true)
         })
     }
 }
